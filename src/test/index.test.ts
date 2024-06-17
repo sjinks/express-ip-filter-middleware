a96ae5ebe4d609bcf66e1b91b6a3bf81a9a60213
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-06-17 04:26:14 +0300

    test: refactor tests, remove mocha

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,4 +1,5 @@
 import { BlockList } from 'node:net';
+import { describe, it } from 'node:test';
 import request from 'supertest';
 import express, { type Application, type NextFunction, type Request, type Response } from 'express';
 import { IPBlockedError, Options, ipFilterMiddleware } from '..';
@@ -15,33 +16,35 @@ function buildServer(options: Options): Application {
     return server;
 }
 
-describe('express-ip-filter-middleware', function (): void {
-    it('should deny everyone in WHITELIST mode with empty lists', function () {
+const promiseVoid = () => Promise.resolve();
+
+void describe('express-ip-filter-middleware', async () => {
+    await it('should deny everyone in WHITELIST mode with empty lists', () => {
         const server = buildServer({ mode: 'whitelist' });
-        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(403);
+        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(403).then(promiseVoid);
     });
 
-    it('should allow everyone in BLACKLIST mode with empty lists', function () {
+    await it('should allow everyone in BLACKLIST mode with empty lists', () => {
         const server = buildServer({ mode: 'blacklist' });
-        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(200);
+        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(200).then(promiseVoid);
     });
 
-    it('should favor allow in BLACKLIST mode', function () {
+    await it('should favor allow in BLACKLIST mode', () => {
         const list = new BlockList();
         list.addAddress('192.168.2.1');
         const server = buildServer({ mode: 'blacklist', allow: list, deny: list });
-        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(200);
+        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(200).then(promiseVoid);
     });
 
-    it('should favor deny in WHITELIST mode', function () {
+    await it('should favor deny in WHITELIST mode', () => {
         const list = new BlockList();
         list.addAddress('192.168.2.1');
 
         const server = buildServer({ mode: 'whitelist', allow: list, deny: list });
-        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(403);
+        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(403).then(promiseVoid);
     });
 
-    it('should take the IP address from ipOverride()', function () {
+    await it('should take the IP address from ipOverride()', () => {
         const allow = new BlockList();
         allow.addAddress('192.168.2.5');
 
@@ -51,24 +54,24 @@ describe('express-ip-filter-middleware', function (): void {
             ipOverride: () => '::ffff:192.168.2.5',
         });
 
-        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(200);
+        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(200).then(promiseVoid);
     });
 
-    it('should fail if ipOverride() returns a bad IP', function () {
+    await it('should fail if ipOverride() returns a bad IP', () => {
         const server = buildServer({
             mode: 'whitelist',
             ipOverride: () => '192.168.2.500',
         });
 
-        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(500);
+        return request(server).get('/').set('X-Forwarded-For', '192.168.2.1').expect(500).then(promiseVoid);
     });
 
-    it('should fail if IP address cannot be determined', function () {
+    await it('should fail if IP address cannot be determined', () => {
         const server = buildServer({
             mode: 'whitelist',
             ipOverride: () => undefined,
         });
 
-        return request(server).get('/').expect(500);
+        return request(server).get('/').expect(500).then(promiseVoid);
     });
 });
